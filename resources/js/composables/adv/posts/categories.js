11b33a90788367c61aeffff6040dac1988d67ace
PC <kornat041@mail.ru>
2024-01-12 14:24:51 +0500

    fixed categories get from api

--- resources/js/composables/adv/posts/categories.js
+++ resources/js/composables/adv/posts/categories.js
@@ -11,7 +11,7 @@ export default function useCategories() {
         api_url = api_url || '/api/categories';
         let origin = window.location.origin;
         let response = await axios.get(origin + api_url)
-        categories.value = response.data;
+        categories.value = response.data.data;
     }
 
 
